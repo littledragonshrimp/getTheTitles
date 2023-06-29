cdb3a323325a184016e19453ff39f32cd509c2d8
Jordan <JDLChang@gmail.com>
2023-06-29 11:59:47 -0500

    adds getTheAuthors

--- getTheTitles.js
+++ getTheTitles.js
@@ -1,6 +1,6 @@
 let booklist = [];
 
-class book {
+class Book {
     constructor(title, author, pages, read) {
         this.title = title;
         this.author = author;
@@ -9,18 +9,21 @@ class book {
     }
 }
 
-booklist.push(new book("The Hobbit", "J.R.R. Tolkien", 295, true));
-booklist.push(new book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, true));
-booklist.push(new book("The Two Towers", "J.R.R. Tolkien", 352, true));
-booklist.push(new book("The Return of the King", "J.R.R. Tolkien", 416, true));
-booklist.push(new book("The Silmarillion", "J.R.R. Tolkien", 365, true));
-booklist.push(new book("The Children of Hurin", "J.R.R. Tolkien", 313, true));
-booklist.push(new book("The Fall of Gondolin", "J.R.R. Tolkien", 304, true));
+booklist.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
+booklist.push(new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, true));
+booklist.push(new Book("The Two Towers", "J.R.R. Tolkien", 352, true));
+booklist.push(new Book("The Return of the King", "J.R.R. Tolkien", 416, true));
+booklist.push(new Book("The Silmarillion", "J.R.R. Tolkien", 365, true));
+booklist.push(new Book("The Children of Hurin", "J.R.R. Tolkien", 313, true));
+booklist.push(new Book("The Fall of Gondolin", "J.R.R. Tolkien", 304, true));
 
 
 
-const getTheTitles = function(bookList) {
-    return booklist.map((book) => book.title);
+const getTheTitles = function(booklist) {
+    return booklist.map((Book) => Book.title);
 };
 
 
+const getTheAuthors = function(booklist){
+    return booklist.map((Book) => Book.author);
+};
